5af1544dcdbf5ec7e590533037f3574d4a00a7f8
Chris Norman <chrisnorman1988@gmail.com>
2018-04-08 23:01:46 +0100

    mvp

--- js/app.js
+++ js/app.js
@@ -1,3 +1,87 @@
-function test(){
+document.addEventListener("DOMContentLoaded", function(event) {
+
+  // Get the modal
+  var aboutModal = document.getElementById('aboutModal');
+  var contactModal = document.getElementById('contactModal');
+  var classOneModal = document.getElementById('classOneModal');
+  // Get the button that opens the modal
+  var about = document.getElementById("about");
+  var contact = document.getElementById("contact");
+  var classOne = document.getElementById("classOne");
+  // Get the <span> element that closes the modal
+  var span1 = document.getElementsByClassName("close1")[0];
+  var span2 = document.getElementsByClassName("close2")[0];
+  var span3 = document.getElementsByClassName("close3")[0];
+
+  // When the user clicks on the button, open the modal
+  about.onclick = function() {
+      aboutModal.style.display = "block";
+  }
+  contact.onclick = function() {
+      contactModal.style.display = "block";
+  }
+  classOne.onclick = function() {
+      classOneModal.style.display = "block";
+  }
+  // When the user clicks on <span> (x), close the modal
+
+  span1.onclick = function(){
+    aboutModal.style.display = "none";
+  }
+  span2.onclick = function(){
+    contactModal.style.display = "none";
+  }
+  span3.onclick = function(){
+    classOneModal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+      if (event.target === aboutModal || event.target === contactModal || event.target === classOneModal) {
+        aboutModal.style.display = "none";
+        contactModal.style.display = "none";
+        classOneModal.style.display = "none";
+      }
+  }
   console.log('here');
-}
+
+  function initMapBattersea() {
+    var battersea = {lat: 51.4744939, lng: -0.1646288};
+    var mapOne = new google.maps.Map(document.getElementById('map-battersea'), {
+      zoom: 14,
+      center: battersea
+    });
+    var marker = new google.maps.Marker({
+      position: battersea,
+      map: mapOne
+    });
+    console.log(mapOne);
+  }
+  initMapBattersea();
+
+  function initMapTooting() {
+    var tooting = {lat: 51.4314844, lng: -0.1410382};
+    var mapTwo = new google.maps.Map(document.getElementById('map-tooting'), {
+      zoom: 14,
+      center: tooting
+    });
+    var marker = new google.maps.Marker({
+      position: tooting,
+      map: mapTwo
+    });
+  }
+  initMapTooting();
+
+  function initMapMatchstick(){
+    var matchstick = {lat: 51.4314844, lng: -0.1410382};
+    var mapThree = new google.maps.Map(document.getElementById('map-matchstick'), {
+      zoom: 14,
+      center: matchstick
+    });
+    var marker = new google.maps.Marker({
+      position: matchstick,
+      map: mapThree
+    });
+  }
+  initMapMatchstick()
+});
